d873c0914823d0ac251bae3877a47a526d878263
Pahirathan Nithilan <60086422+Pahinithi@users.noreply.github.com>
2024-09-04 06:52:01 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import json
+
+import streamlit as st
+from groq import Groq
+
+
+# streamlit page configuration
+st.set_page_config(
+    page_title="Ceylon AI Chat",
+    page_icon="👋",
+    layout="centered"
+)
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+config_data = json.load(open(f"{working_dir}/config.json"))
+
+GROQ_API_KEY = config_data["GROQ_API_KEY"]
+
+# save the api key to environment variable
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+client = Groq()
+
+# initialize the chat history as streamlit session state of not present already
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+
+# streamlit page title
+st.title("💬 🇱🇰 Ceylon AI Chat")
+
+# display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# input field for user's message:
+user_prompt = st.chat_input("Ask Ceylon AI Chat .....")
+
+if user_prompt:
+
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+    # sens user's message to the LLM and get a response
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant"},
+        *st.session_state.chat_history
+    ]
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=messages
+    )
+
+    assistant_response = response.choices[0].message.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    # display the LLM's response
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
+
